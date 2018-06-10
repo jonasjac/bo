08ef7dbfd9f77cc93b9a2ab1c9ce7fcea084cba2
Jonas <33356021+jonasjac@users.noreply.github.com>
2018-06-10 09:36:48 +0200

    Update purge.js

--- src/commands/purge.js
+++ src/commands/purge.js
@@ -28,7 +28,7 @@ module.exports = class PurgeCommand {
   get props () {
     return {
       triggers: ['purge', 'clean', 'clear'],
-      usage: '{command} <count> [@user] [#channel]',
+      usage: 'purge <count> [@user] [#channel]',
       description: 'Clean an amount of messages. Arguments can be in any order.'
     };
   }
